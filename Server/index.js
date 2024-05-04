90cbc5642b33722265478224f6684593e2ef29f5
DaevernGanendrah <daevernganendrah31@gmail.com>
2024-05-05 04:49:32 +0800

    Add server directory

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,38 @@
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const authRoutes = require('./routes/authRoutes');
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//     app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+//   })
+//   .catch(err => console.error(err));
+
+// // Routes
+// app.use('/api/auth', authRoutes);
+
+// // The remaining routes can be set up in a similar fashion.
+
+
+
+
+
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const path = require('path');
 
 const app = express();
 
@@ -17,7 +47,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   })
   .catch(err => console.error(err));
 
-// Routes
+// API Routes
 app.use('/api/auth', authRoutes);
 
-// The remaining routes can be set up in a similar fashion.
+// Serve Static Files - Make sure this comes after your API routes
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
