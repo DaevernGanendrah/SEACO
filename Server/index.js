b404df1b4be595a6cdb8b9f14fd2e32b05fc7bb4
DaevernGanendrah <daevernganendrah31@gmail.com>
2024-05-05 12:51:36 +0800

    Add Server  directory7

--- Server/index.js
+++ Server/index.js
@@ -67,6 +67,56 @@
 
 
 
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const path = require('path');
+// const authRoutes = require('./routes/authRoutes');
+
+// // const app = express();
+
+// // app.use(cors());
+// const cors = require('cors');
+// app.use(cors({
+//     origin: 'https://seaco.onrender.com' // Adjust this to match the origin from which you expect to call the API
+// }));
+
+// app.use(express.json());
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//     app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+//   })
+//   .catch(err => console.error(err));
+
+// // API Routes
+// app.use('/api/auth', authRoutes);
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// // The "catchall" handler: for any request that doesn't match one above, send back index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
