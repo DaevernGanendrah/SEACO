9e9a6bebd86582b2f4e99109ed1d41ee4652c67a
DaevernGanendrah <56118196+DaevernGanendrah@users.noreply.github.com>
2024-05-21 01:27:12 +0800

    Update5 Page3.js

--- Client/src/components/Page3.js
+++ Client/src/components/Page3.js
@@ -28,8 +28,8 @@ function Page3() {
       <div className="buttons-container">
         {/* <button className="inputBx3">Community Briefs</button> */}
         <button onClick={() => navigate('/dashboard')} className="inputBx3">Community Briefs</button>
-        <button className="inputBx3">Health Record 2013</button>
-        <button className="inputBx3">Health Record 2018</button>
+        <button className="inputBx3">Health Round 2013</button>
+        <button className="inputBx3">Health Round 2018</button>
       </div>
     </div>
   );
