71f2d94cedfc89b4e09990276f3820c3d40c4904
DaevernGanendrah <56118196+DaevernGanendrah@users.noreply.github.com>
2024-11-28 12:29:41 +0800

    Update6 App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -143,6 +143,7 @@ import GlobeDashboard from './components/GlobeDashboard';
 import Registration from './components/Registration';
 import Page3 from './components/Page3';
 import HealthChart2013 from './components/HealthChart2013';
+import HealthChart2018 from './components/HealthChart2018';
 
 const App = () => {
   return (
@@ -151,6 +152,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/healthchart2013" element={<HealthChart2013 />} />
+        <Route path="/healthchart2018" element={<HealthChart2018 />} />
         <Route path="/globedashboard" element={<GlobeDashboard />} />
         <Route path="/register" element={<Registration />} />
         <Route path="/page3" element={<Page3 />} />
