3a02d8e715e667ef5db482dd46b41b75df6118de
DaevernGanendrah <56118196+DaevernGanendrah@users.noreply.github.com>
2024-11-29 11:05:56 +0800

    added healthRoutes.js

---
+++ Server/routes/healthRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { Health2018, Health2013 } = require('../models/healthModel');
+
+const router = express.Router();
+
+// Get 2018 data
+router.get('/2018', async (req, res) => {
+  try {
+    const data = await Health2018.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching 2018 data', error });
+  }
+});
+
+// Get 2013 data
+router.get('/2013', async (req, res) => {
+  try {
+    const data = await Health2013.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching 2013 data', error });
+  }
+});
+
+module.exports = router;
