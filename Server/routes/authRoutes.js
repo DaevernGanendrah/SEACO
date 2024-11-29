dfa36e40eef3223528d96aa6b1afc6cdbee83639
DaevernGanendrah <56118196+DaevernGanendrah@users.noreply.github.com>
2024-11-29 12:50:35 +0800

    Update authRoutes.js

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken');
 
-const router = express.Router();
+// const router = express.Router();
 
 
 // router.post('/register', async (req, res) => {
