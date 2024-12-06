07806f9bd078a489f2e906f97539e12c28035b7d
DaevernGanendrah <56118196+DaevernGanendrah@users.noreply.github.com>
2024-12-06 14:42:31 +0800

    Update healthModels.js

--- Server/models/healthModels.js
+++ Server/models/healthModels.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const healthSchema = new mongoose.Schema({}, { strict: false }); // Flexible schema
 
-const Health2018 = mongoose.model('HealthRound2018', healthSchema, 'HealthRound2018');
-const Health2013 = mongoose.model('HealthRound2013', healthSchema, 'HealthRound2013');
+const HealthRound2018 = mongoose.model('HealthRound2018', healthSchema, 'HealthRound2018');
+const HealthRound2013 = mongoose.model('HealthRound2013', healthSchema, 'HealthRound2013');
 
 module.exports = { HealthRound2018, HealthRound2013 };
